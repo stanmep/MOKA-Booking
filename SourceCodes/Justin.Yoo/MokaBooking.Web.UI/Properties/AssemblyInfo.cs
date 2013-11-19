d427b65d5363599c4ffa62c6daf850e0a189dd07
Justin Yoo <justin.yoo@aliencube.com>
2013-11-20 00:08:26 +1100

    Updated AssemblyInfo.cs for both projects.

--- SourceCodes/Justin.Yoo/MokaBooking.Web.UI/Properties/AssemblyInfo.cs
+++ SourceCodes/Justin.Yoo/MokaBooking.Web.UI/Properties/AssemblyInfo.cs
@@ -8,10 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MokaBooking.Web.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MokaBooking.Web.UI")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,15 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bf13c9a7-f445-4204-85cb-ce59073a3f08")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
