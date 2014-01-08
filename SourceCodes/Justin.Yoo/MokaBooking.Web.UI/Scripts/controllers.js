6f3766f1707c723de2437e8ecebe7e9c46e6debf
Justin Yoo <justin.yoo@aliencube.com>
2014-01-08 23:37:34 +1100

    Updated controller.js.

--- SourceCodes/Justin.Yoo/MokaBooking.Web.UI/Scripts/controllers.js
+++ SourceCodes/Justin.Yoo/MokaBooking.Web.UI/Scripts/controllers.js
@@ -1,47 +1,4 @@
-﻿var bookings = [{
-        "givenName": "Edward",
-        "surname": "Kim",
-        "email": "edward@mokabooking.com",
-        "bookingDate": "2013-12-12, 18:00",
-        "numberOfGuests": 2
-    },
-    {
-        "givenName": "Hayley",
-        "surname": "Heo",
-        "email": "hayley@mokabooking.com",
-        "bookingDate": "2013-12-13, 18:00",
-        "numberOfGuests": 2
-    },
-    {
-        "givenName": "Jiman",
-        "surname": "Kim",
-        "email": "jiman@mokabooking.com",
-        "bookingDate": "2013-12-13, 20:00",
-        "numberOfGuests": 4
-    },
-    {
-        "givenName": "Justin",
-        "surname": "Yoo",
-        "email": "justin@mokabooking.com",
-        "bookingDate": "2013-12-14, 18:00",
-        "numberOfGuests": 4
-    },
-    {
-        "givenName": "Robin",
-        "surname": "Kang",
-        "email": "robin@mokabooking.com",
-        "bookingDate": "2013-12-14, 20:00",
-        "numberOfGuests": 2
-    },
-    {
-        "givenName": "Sunju",
-        "surname": "Choi",
-        "email": "sunju@mokabooking.com",
-        "bookingDate": "2013-12-14, 20:00",
-        "numberOfGuests": 6
-    }];
-
-var availabilities = [{
+﻿var availabilities = [{
         "bookingDate": "2014-01-01, 20:00",
         "booked": true
     },
@@ -210,7 +167,11 @@ var availabilities = [{
     });
 
     module.controller("bookingController", function ($scope, $http) {
-        $scope.bookings = bookings;
+        $http
+            .get("/api/bookings", { "responseType": "json" })
+            .success(function(results) {
+                $scope.bookings = results;
+            });
     });
 
     module.controller("availabilityController", function ($scope, $http) {
